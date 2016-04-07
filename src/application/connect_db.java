21d8f4bda4ea89933cc53691471a2c9e8e7714ec
Michael de Klein <0906393@hr.nl>
2016-04-07 09:54:19 +0200

    connect_db connection to local database established

--- src/application/connect_db.java
+++ src/application/connect_db.java
@@ -1,11 +1,30 @@
 package application;
 
+import java.sql.*;
+
 /**
  * Created by Michael on 6-4-2016.
  */
 
 public class connect_db {
     public static void main(String[] args) {
+        try {
+            // verbinden met database
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/indice", "root", "");
 
+            // statement maken
+            Statement myStmt = myConn.createStatement();
+
+            // SQL query uitvoeren
+            ResultSet myRs = myStmt.executeQuery("select * from veiligheidsindice");
+
+            // data verwerken
+            while (myRs.next()) {
+                System.out.println(myRs.getString("Gebied") + ", " + myRs.getString("Jaar") + ", " + myRs.getString("Cijfer"));
+            }
+        }
+        catch (Exception exc) {
+            exc.printStackTrace();
+        }
     }
 }
